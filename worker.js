e446717103fdd8227ebbcdb82ff91cca4b12fa27
Nathan Clevenger <nateclev@gmail.com>
2022-11-30 06:30:21 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,18 +1,18 @@
 export const api = {
   icon: '🚀',
-  name: 'templates.do',
-  description: 'Cloudflare Worker Template',
-  url: 'https://templates.do/api',
-  type: 'https://apis.do/templates',
+  name: 'tsv.do',
+  description: 'TSV Generation & Transformation API',
+  url: 'https://tsv.do/api',
+  type: 'https://apis.do/formats',
   endpoints: {
-    listCategories: 'https://templates.do/api',
-    getCategory: 'https://templates.do/:type',
+    listCategories: 'https://tsv.do/api',
+    getCategory: 'https://tsv.do/:type',
   },
-  site: 'https://templates.do',
-  login: 'https://templates.do/login',
-  signup: 'https://templates.do/signup',
-  subscribe: 'https://templates.do/subscribe',
-  repo: 'https://github.com/drivly/templates.do',
+  site: 'https://tsv.do',
+  login: 'https://tsv.do/login',
+  signup: 'https://tsv.do/signup',
+  subscribe: 'https://tsv.do/subscribe',
+  repo: 'https://github.com/drivly/tsv.do',
 }
 
 export const gettingStarted = [
@@ -21,7 +21,7 @@ export const gettingStarted = [
 ]
 
 export const examples = {
-  listItems: 'https://templates.do/worker',
+  listItems: 'https://tsv.do/worker',
 }
 
 export default {
